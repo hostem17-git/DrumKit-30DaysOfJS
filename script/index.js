e6254249a76276e5caecb50f4e2c76209186ff80
hostem17-git <vjjais17@gmail.com>
2021-03-05 21:41:29 +0530

    Added mouse click feature

--- script/index.js
+++ script/index.js
@@ -10,71 +10,89 @@ var tink =  new Audio("sounds/tink.wav");
 var tom =  new Audio("sounds/tom.wav");
 
 
+function playSound(key){
+  console.log("playing "+ key);
+  switch(key){
+    case 'a':
+    case 'A':
+      clap.play();
+      break;
+    case 's':
+    case 'S':
+      hihat.play();
+      break;
+    case 'd':
+    case 'D':
+      kick.play();
+      break;
+    case 'e':
+    case 'E':
+      openhat.play();
+      break;
+    case 'f':
+    case 'F':
+      openhat.play();
+      break;
+    case 'g':
+    case 'G':
+      boom.play();
+      break;
+    case 'h':
+    case 'H':
+      ride.play();
+      break;
+    case 'j':
+    case 'J':
+      snare.play();
+      break;
+    case 'k':
+    case 'K':
+      tom.play();
+      break;
+    case 'l':
+    case 'L':
+      tink.play();
+      break;
+  }
+}
+
+
 function blink(key){
-  key= key.toLowerCase();
-  console.log(key);
-  console.log(`div[data-key="${key}"]`)
-  
-  document.querySelector(`div[data-key="${key}"]`).style.boxShadow = "0 0 30px white";
-  setTimeout(function(){
-    document.querySelector(`div[data-key="${key}"]`).style.boxShadow = null;  
-    console.log("fome");
-  },10);
-  
-  
+  if(key ==='a' || key === "A" || key ==='s' || key === "S" || key ==='d' || key === "D" || key ==='f' || key === "F" || key ==='g' || key === "G" || key ==='h' || key === "H" || key ==='j' || key === "J" || key ==='k' || key === "K" || key ==='l' || key === "L"){
+    key= key.toLowerCase();
+    console.log(key);
+    console.log(`div[data-key="${key}"]`)
+    document.querySelector(`div[data-key="${key}"]`).style.boxShadow = "0 0 30px white";
+    setTimeout(function(){
+      document.querySelector(`div[data-key="${key}"]`).style.boxShadow = null;  
+      console.log("fome");
+    },10); 
+}
 }
 
 
 
 var lists = document.querySelectorAll('div[data-key]');
 lists.forEach(function(item){
-  item.addEventListener("click",function(e){
-    console.log(e);
+  item.addEventListener("click",function(event){
+    console.log(event.path[1].getAttribute("data-key"));
+    var keyPressed = event.path[1].getAttribute("data-key");
+    if(keyPressed != null){
+      playSound(keyPressed);
+      blink(keyPressed);
+    }
+
   });
 });
   
+
 
 document.addEventListener("keydown",function(e){
   var key = e.key;
   console.log("pressed " + key)
   document.getElementById("test").innerHTML = "TEST Success"
-  if(key =='a' || key == "A"){
-    console.log("played")
-    clap.play();
-    blink(key);
-  }
-  else if(key =='s' || key == "S"){
-    console.log("played")
-    hihat.play();
-    blink(key);
-  }
-  else if(key =='d' || key == "D"){
-    console.log("played")
-    kick.play();
-    blink(key);
-  }
-  else if(key =='f' || key == "F"){
-    openhat.play();
-    blink(key);
-  }
-  else if(key =='g' || key == "G"){
-    boom.play();
-    blink(key);
-  }
-  else if(key =='h' || key == "H"){
-    ride.play()
-    blink(key);
-  }
-  else if(key =='j' || key == "J"){
-    snare.play();
-    blink(key);
-  }
-  else if(key =='k' || key == "K"){
-    tom.play();
-    blink(key);
-  }
-  else if(key =='l' || key == "L"){
-    tink.play();
-    blink(key);
-  }
+  playSound(key);
+  blink(key);
+  console.log("played "+key);
+
 },false);
